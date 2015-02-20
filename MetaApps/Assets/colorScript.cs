2b6eb5998f579dc30ab91317dfbf194dfc120d8d
kerama19 <kenneth.ramirez@proximitycr.com>
2015-02-20 17:05:10 -0600

    Cube Color App

---
+++ MetaApps/Assets/colorScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class colorScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void printCube()
+	{
+		Color randomColor = new Color (Random.Range (0.0f, 1.0f), Random.Range (0.0f, 1.0f), Random.Range (0.0f, 1.0f), 1.0f);
+		renderer.material.color = randomColor;
+	}
+}
